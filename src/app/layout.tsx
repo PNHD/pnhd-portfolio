5a40db1fa3f2b12749dfb7b5a085854407e61f79
PNHD <phamnhathaidang@gmail.com>
2026-08-07 18:06:33 +0700

    Load accessibility safeguards globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import "./portfolio-refresh.css";
 import "./project-cases.css";
 import "./project-cases-expanded.css";
+import "./accessibility.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
